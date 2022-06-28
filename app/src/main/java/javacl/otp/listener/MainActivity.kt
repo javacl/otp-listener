1f4c244e969de3de36caec6127042f3a7930bd6c
sadeghi <javacl.sadeghi@gmail.com>
2022-06-28 13:12:09 +0430

    finished otp code

--- app/src/main/java/javacl/otp/listener/MainActivity.kt
+++ app/src/main/java/javacl/otp/listener/MainActivity.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import javacl.otp.listener.ui.theme.OtplistenerTheme
+import javacl.otp.listener.ui.theme.OtpListenerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OtplistenerTheme {
+            OtpListenerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
--- app/src/main/java/javacl/otp/listener/ui/theme/Theme.kt
+++ app/src/main/java/javacl/otp/listener/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun OtplistenerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun OtpListenerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
